6596740540e9172ab67288993da0a8cf34f34708
jhassler1031 <jhassler1031@gmail.com>
2018-07-12 13:45:39 -0400

    added GET and POST functionality to John's Page

--- src/components/JohnsData.js
+++ src/components/JohnsData.js
@@ -8,15 +8,58 @@ class JohnsData extends Component {
       johnsData: []
     };
 
+    this._postData = this._postData.bind(this);
+
   }
 
+  // GET method ================================================================
   componentDidMount() {
     let self = this;
+
+    fetch('http://tiny-lasagna-server.herokuapp.com/collections/johnhassler')
+    .then((response)=>{
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((responseAsJson)=>{
+      self.setState({johnsData: responseAsJson});
+    })
+    .catch((error)=>{
+      console.log("There was a problem: \n", error);
+    });
+  }
+
+  // POST method ===============================================================
+  _postData() {
+    let context = {
+      beerName: "New Belgium la Folie",
+      type: "Pure Awesome",
+      whereItBelongs: "In ma belly!"
+    };
+
+    fetch('http://tiny-lasagna-server.herokuapp.com/collections/johnhassler',{
+      method: "POST",
+      body: JSON.stringify(context),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+    .then((response)=>{
+      console.log(response);
+    })
+    .catch((error)=>{
+      console.log("There was a problem: \n", error);
+    });
   }
 
   render() {
     return (
-      <p>John's Data</p>
+      <div className="johnsPage">
+        <p>John's Data</p>
+        <input type="button" value="Add Beer" onClick={this._postData}/>
+      </div>
     );
   }
 }
